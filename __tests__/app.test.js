396587fa4f76cf8a294607ebddfbcef347445deb
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-06-24 10:00:45 -0700

    first test passing for redirecting user on login

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,15 +4,20 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 
-jest.mock('../lib/utils/github');
+jest.mock('../lib/services/github');
 
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('should redirect user to github oauth for login', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+    console.log('res.header.location', res.header.location);
+    expect(res.header.location).toMatch(`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=user&redirect_uri=http://localhost:7890/api/v1/github/callback`);
+
+
   });
   afterAll(() => {
     pool.end();
