afab2da9e5d20a448987b45f05c41a82139d3dac
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-06-24 11:43:42 -0700

    second test passing for redirecting user to dashboard

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,9 +16,24 @@ describe('backend-express-template routes', () => {
     const res = await request(app).get('/api/v1/github/login');
     console.log('res.header.location', res.header.location);
     expect(res.header.location).toMatch(`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=user&redirect_uri=http://localhost:7890/api/v1/github/callback`);
-
-
   });
+
+  it('should login and redirect users to /api/v1/github/dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
